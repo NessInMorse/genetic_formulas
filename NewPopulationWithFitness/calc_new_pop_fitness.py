caf2b44e10e86c625fdabc1247b122d34de34f1a
Marc <56824499+NessInMorse@users.noreply.github.com>
2021-12-09 14:09:41 +0100

    Create calc_new_pop_fitness.py

---
+++ NewPopulationWithFitness/calc_new_pop_fitness.py
@@ -0,0 +1,24 @@
+def calculateNewFreq(hom_dom, het, hom_rec, mode):
+        return abs(float(f"{bool(mode=='p') - (hom_rec + (1/2) * het) / (hom_dom + het + hom_rec):.3f}"))
+
+def grantGeneticPassing(hom_dom, het, hom_rec, live_chance):
+        hom_dom = hom_dom * live_chance[0]
+        het = het * live_chance[1]
+        hom_rec = hom_rec * live_chance[2]
+        return hom_dom, het, hom_rec
+
+
+def calcAllelFreq(dom_freq, rec_freq):
+        hom_dom = dom_freq ** 2
+        het = 2 * dom_freq * rec_freq
+        hom_rec = rec_freq ** 2
+        return hom_dom, het, hom_rec
+
+if __name__ == "__main__":
+        dom_freq = 0.5
+        rec_freq = (1 - dom_freq)
+        fitness = [1, 0.9, 0.8]
+        hom_dom, het, hom_rec = calcAllelFreq(dom_freq, rec_freq)
+        hom_dom, het, hom_rec = grantGeneticPassing(hom_dom, het, hom_rec, fitness)
+        newfreq_q = calculateNewFreq(hom_dom, het, hom_rec, "q")
+        print(f"Frequences for each allel:\nP: {(1 - newfreq_q):.3f}\nq: {newfreq_q}")
